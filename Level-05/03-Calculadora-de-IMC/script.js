43fd0b00b43990441b16fcc492f74691ec394609
Pedro Vasconcellos <pedrovas.dev@gmail.com>
2023-08-28 14:45:02 -0300

    código refatorado

--- Level-05/03-Calculadora-de-IMC/script.js
+++ Level-05/03-Calculadora-de-IMC/script.js
@@ -1,33 +1,60 @@
 // variables
 const form = document.querySelector("form");
-const inputWeight = document.querySelector("#weight");
-const inputHeight = document.querySelector("#height");
+// const inputWeight = document.querySelector("#weight");
+// const inputHeight = document.querySelector("#height");
+
 const alertPop = document.querySelector(".alert-error");
-const modalWrapper = document.querySelector(".modal-wrapper");
-const modalButton = document.querySelector('.modal button')
 let txtPopUp = document.querySelector(".modal.card h2");
 
-// events 
-modalButton.addEventListener('click', closePopUp)
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modalButton = document.querySelector(".modal button");
+
+// Variável usada para cancelar o intervalo anterior fazendo com que sempre tenha 5s o popup
+let alertInterval;
+
+const Modal = {
+  weight: document.querySelector("#weight"),
+  height: document.querySelector("#height"),
+
+  open() {
+    modalWrapper.classList.add("open");
+  },
+  close() {
+    modalWrapper.classList.remove("open");
+  },
+};
+
+// events
+modalButton.onclick = closePopUp;
 
 // functions
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  const weight = Number(inputWeight.value);
-  const height = Number(inputHeight.value);
+  const weight = Number(Modal.weight.value);
+  const height = Number(Modal.height.value);
 
+  // alerta de erro
   if (weight == "" || height == "") {
     alertPop.classList.add("open");
+    alertPopTimer();
   } else {
+    // sem errro, calcula o IMC
     let imc = (weight / (height / 100) ** 2).toFixed(2);
-    modalWrapper.classList.add("open");
-    txtPopUp.innerText = `Seu IMC é ${imc}`
+    Modal.open();
+    txtPopUp.innerText = `Seu IMC é ${imc}`;
   }
-}
+};
 
 function closePopUp() {
-  modalWrapper.classList.remove('open')
-  inputHeight.value = ''
-  inputWeight.value = ''
+  Modal.close();
+  Modal.height.value = "";
+  Modal.weight.value = "";
+}
+
+function alertPopTimer() {
+  clearTimeout(alertInterval);
+  alertInterval = setTimeout(() => {
+    alertPop.classList.remove("open");
+  }, 5000);
 }
